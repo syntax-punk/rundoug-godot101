531c24bd780acd1d801f2662bf67a43a1ef2cc3d
syntax-punk <david.jaeren@gmail.com>
2024-10-24 23:30:54 +0200

    Having fun with navigation

--- Player.cs
+++ Player.cs
@@ -11,11 +11,62 @@ public partial class Player : Area2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		ScreenSize = GetViewport().Size;
+		ScreenSize = GetViewportRect().Size;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		var velocity = Vector2.Zero; // The player's movement vector.
+
+		if (Input.IsActionPressed("moveRight"))
+		{
+			velocity.X += 1;
+		}
+
+		if (Input.IsActionPressed("moveLeft"))
+		{
+			velocity.X -= 1;
+		}
+
+		if (Input.IsActionPressed("moveDown"))
+		{
+			velocity.Y += 1;
+		}
+
+		if (Input.IsActionPressed("moveUp"))
+		{
+			velocity.Y -= 1;
+		}
+
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+		if (velocity.Length() > 0)
+		{
+			velocity = velocity.Normalized() * Speed;
+			animatedSprite2D.Play();
+		}
+		else
+		{
+			animatedSprite2D.Stop();
+		}
+
+		Position += velocity * (float)delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+		);
+
+		if (velocity.X != 0)
+		{
+			animatedSprite2D.Animation = "walk";
+			animatedSprite2D.FlipV = false;
+			animatedSprite2D.FlipH = velocity.X < 0;
+		}
+		else if (velocity.Y != 0)
+		{
+			animatedSprite2D.Animation = "up";
+			animatedSprite2D.FlipV = velocity.Y > 0;
+		}
 	}
 }
