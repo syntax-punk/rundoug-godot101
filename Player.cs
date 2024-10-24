9860e22142d9eb1d5c4c9d7414928a26e3031a14
syntax-punk <david.jaeren@gmail.com>
2024-10-24 23:00:18 +0200

    Playing with godot

---
+++ Player.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class Player : Area2D
+{
+	[Export]
+	public int Speed { get; set; } = 400;
+
+	public Vector2 ScreenSize;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		ScreenSize = GetViewport().Size;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
